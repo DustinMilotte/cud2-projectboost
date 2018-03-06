24fd84ce0d795aea9c79411fe721b5cb60536d19
Dustin Milotte <dmilotte@gmail.com>
2018-03-05 23:36:14 -0500

    add audio clips for success and death

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,75 +6,95 @@ public class Rocket : MonoBehaviour {
     bool rotatingRight = false;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip newLevelSound;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-    enum State {Alive, Dead, Transcending}
+    enum State { Alive, Dying, Transcending }
     State state = State.Alive;
 
-    void Start () {
-        rigidBody = GetComponent<Rigidbody> ();
-        audioSource = GetComponent<AudioSource> ();
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    void Update () {
-        if(state == State.Alive){
-            Thrust ();
-            Rotate ();
+    void Update() {
+        if (state == State.Alive) {
+            RespondToThrust();
+            RespondToRotate();
         }
     }
-    
-    void OnCollisionEnter (Collision other) {
-        if(state != State.Alive) { return; }
+
+    void OnCollisionEnter(Collision other) {
+        if (state != State.Alive) { return; }
         switch (other.gameObject.tag) {
             case "Friendly":
-                print ("Friendly");
                 break;
             case "Finish":
-                state = State.Transcending;
-                Invoke("LoadNextLevel", 1f);
+                StartSuccessSequence();
                 break;
             default:
-                state = State.Transcending;
-                Invoke("LoadFirstLevel", 1f);
+                StartDeathSequence();
                 break;
         }
     }
 
-    private void LoadFirstLevel(){
+    private void StartSuccessSequence() {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(newLevelSound);
+        Invoke("LoadNextLevel", 1f);
+    }
+
+    private void StartDeathSequence() {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+
+    private void LoadFirstLevel() {
         SceneManager.LoadScene(0);
     }
 
-    private void LoadNextLevel(){
+    private void LoadNextLevel() {
         SceneManager.LoadScene(1);
     }
 
-    private void Thrust () {
-        if (Input.GetKey (KeyCode.Space)) {
-            rigidBody.AddRelativeForce (Vector3.up * mainThrust);
-            if (!audioSource.isPlaying) {
-                audioSource.Play ();
-            }
+    private void RespondToThrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            ApplyThrust();
         } else {
-            audioSource.Stop ();
+            audioSource.Stop();
         }
     }
 
-    private void Rotate () {
+    private void ApplyThrust() {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotate() {
         rigidBody.freezeRotation = true;
         float rotationThisFrame = rcsThrust * Time.deltaTime;
-        if (Input.GetKey (KeyCode.A) && !rotatingRight) {
-            transform.Rotate (Vector3.forward * rotationThisFrame);
+        if (Input.GetKey(KeyCode.A) && !rotatingRight) {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
             rotatingLeft = true;
         }
-        if (Input.GetKeyUp (KeyCode.A)) {
+        if (Input.GetKeyUp(KeyCode.A)) {
             rotatingLeft = false;
         }
-        if (Input.GetKey (KeyCode.D) && !rotatingLeft) {
-            transform.Rotate (-Vector3.forward * rotationThisFrame);
+        if (Input.GetKey(KeyCode.D) && !rotatingLeft) {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
             rotatingRight = true;
         }
-        if (Input.GetKeyUp (KeyCode.D)) {
+        if (Input.GetKeyUp(KeyCode.D)) {
             rotatingRight = false;
         }
         rigidBody.freezeRotation = false;
