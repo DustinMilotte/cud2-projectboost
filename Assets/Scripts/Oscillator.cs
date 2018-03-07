38712f0bca1ed2adc1bbbace6fcb34ed4f71e994
Dustin Milotte <dmilotte@gmail.com>
2018-03-06 22:43:30 -0500

    add particles, organize folders, add obstacle movement

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+
+    void Start() {
+        startingPos = transform.position;
+    }
+
+    void Update() {
+        if (period != 0f) {
+            float cycles = Time.time / period;
+
+            const float tau = Mathf.PI * 2;
+            float rawSine = Mathf.Sin(cycles * tau);
+
+            movementFactor = (rawSine / 2) + 0.5f;
+            Vector3 offset = movementFactor * movementVector;
+
+            transform.position = startingPos + offset;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+    bool rotatingLeft = false;
+    bool rotatingRight = false;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip newLevelSound;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        if (state == State.Alive) {
+            RespondToThrust();
+            RespondToRotate();
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (state != State.Alive) { return; }
+        switch (other.gameObject.tag) {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence() {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(newLevelSound);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartDeathSequence() {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+    private void RespondToThrust() {
+        if (Input.GetKey(KeyCode.Space)) {
+            ApplyThrust();
+        } else {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust() {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+    private void RespondToRotate() {
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A) && !rotatingRight) {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+            rotatingLeft = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A)) {
+            rotatingLeft = false;
+        }
+        if (Input.GetKey(KeyCode.D) && !rotatingLeft) {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+            rotatingRight = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D)) {
+            rotatingRight = false;
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+}
